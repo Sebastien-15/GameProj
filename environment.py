14b21f5643fa615cef4bcea976b406f39e7a26ab
Sebastien-15 <totobiday@gmail.com>
2025-01-19 03:53:00 -0500

    background improv

--- environment.py
+++ environment.py
@@ -1,11 +1,10 @@
 import pygame
-import os
 import math
 import random
 from config import *
 
 class Block(pygame.sprite.Sprite):
-    def __init__(self, game, x, y, walkthrough, asset_type):
+    def __init__(self, game, x, y, has_sides = True, walkthrough = False, asset_type = -1, border = False, intersection = False, rotation = 'standard'):
         self.game = game
         if walkthrough:
             self._layer = WALKTHROUGH_LAYER
@@ -16,21 +15,38 @@ class Block(pygame.sprite.Sprite):
         self.width = BLOCK_WIDTH
         self.height = BLOCK_HEIGHT
         self.walkthrough = walkthrough
-        # Image of the block=
 
         self.image = pygame.Surface([self.width, self.height])
-        if self.walkthrough:
-            self.image.fill(GRAY)
-        else: 
-            self.image.blit(self.game.block_images[random.randint(0, len(self.game.block_images) - 1)])
-        self.image.set_colorkey(BLACK)
+        if not has_sides:
+            self.image.fill((118, 87, 65))
+        else:
+            if walkthrough:
+                self.image.fill(GRAY)
+            elif not border and not intersection:
+                self.image.blit(self.game.block_images['regular'][random.randint(0, len(self.game.block_images) - 1)])
+            elif border:
+                self.image.blit(self.game.block_images['border'])
+            elif intersection:
+                self.image.blit(self.game.block_images['intersection'])
 
         # Rectangle of the block
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
-        if asset_type == 10: 
+        if asset_type >= 0: 
             Vegetation(self.game, asset_type, self.rect.x, self.rect.y)
+            
+        match rotation:
+            case 'standard':
+                pass
+            case 'left':
+                self.image = pygame.transform.rotate(self.image, 90)
+            case 'upside_down':
+                self.image = pygame.transform.rotate(self.image, 180)
+            case 'right':
+                self.image = pygame.transform.rotate(self.image, 270)
+        self.image.set_colorkey(BLACK)
+            
     
 
 class Border(pygame.sprite.Sprite):
@@ -82,26 +98,32 @@ class Vegetation(pygame.sprite.Sprite):
     def __init__(self, game, asset_type, x, y):
         self.game = game
         self.groups = self.game.all_sprites
+        self._layer = 1
         pygame.sprite.Sprite.__init__(self, self.groups)
         self.frame = 0
         self.asset_type = asset_type
         self.last_update = pygame.time.get_ticks()
         
         match asset_type:
-            case 10:
+            case 0:
                 self.width = TREE_WIDTH
                 self.height = TREE_HEIGHT
+                self.animation_speed = 125
+            case 1:
+                self.width = BUSH_WIDTH
+                self.height = BUSH_HEIGHT
+                self.animation_speed = 150
         
-                self.image = pygame.Surface([self.width, self.height])
-                self.image.set_colorkey(BLACK)
-                self.image.blit(self.game.vegetation_images[asset_type][self.frame], (0, 0))
-                
-                self.rect = self.image.get_rect()
-                self.rect.x = x 
-                self.rect.y = y - self.height 
+        self.image = pygame.Surface([self.width, self.height])
+        self.image.set_colorkey(BLACK)
+        self.image.blit(self.game.vegetation_images[asset_type][self.frame], (0, 0))
+        
+        self.rect = self.image.get_rect()
+        self.rect.x = x - (self.width / 2)
+        self.rect.y = y - self.height 
         
     def animate(self):
-        if pygame.time.get_ticks() - self.last_update > 125:
+        if pygame.time.get_ticks() - self.last_update > self.animation_speed:
             self.frame +=1
             self.last_update = pygame.time.get_ticks()
             if self.frame == len(self.game.vegetation_images[self.asset_type]) - 1:
@@ -109,5 +131,49 @@ class Vegetation(pygame.sprite.Sprite):
         self.image = self.game.vegetation_images[self.asset_type][self.frame]
                 
     def update(self):
-        if self.asset_type == 10:
-            self.animate()
+        self.animate()
+
+class Background_rectangles(pygame.sprite.Sprite):
+    def __init__(self, game):
+        self.game = game
+        self._layer = 0
+        self.groups = self.game.all_sprites
+        pygame.sprite.Sprite.__init__(self, self.groups)
+        self.size = random.choice([20, 20, 20, 20, 20, 80, 80, 80, 80, 160,])
+        self.rotation = 0
+        self.rotation_speed = 2
+        self.last_update = pygame.time.get_ticks()
+        self.fall_speed = random.randint(3, 5)
+        
+        self.image = pygame.Surface([self.size, self.size])
+        self.image.fill(WHITE)
+        self.image.set_colorkey(WHITE)
+        self.rect = self.image.get_rect()
+        self.rect.x = random.randint(-400, WIN_WIDTH + 400)
+        self.rect.y = -self.size
+        self.original = self.image
+        
+        self.rectangle = pygame.Rect((0, 0, self.size, self.size))
+
+        pygame.draw.rect(self.image, BLACK, self.rectangle, random.choice([4, 4, 4, 10, 10, 10, 10, 10, 10, 10,]))
+    
+    def update(self):
+        self.rotate()
+        self.movement()
+        if self.rect.y >= WIN_HEIGHT + self.size:
+            self.kill()
+        
+    def rotate(self):
+        if pygame.time.get_ticks() - self.last_update >= 10:
+            old_center = self.rect.center
+            self.rotation = (self.rotation + self.rotation_speed) % 360 
+            self.image = pygame.transform.rotate(self.original, self.rotation)
+            self.rect = self.image.get_rect()
+            self.last_update = pygame.time.get_ticks()
+            self.rect.center = old_center
+    
+    def movement(self):
+        self.rect.y += self.fall_speed
+        
+        
+        
--- main.py
+++ main.py
@@ -14,13 +14,15 @@ class Game:
         self.FPS = 0
         self.running = True
         self.bg_movement = 0
-        self.bg_name = 'BG2'
+        self.bg_name = 'BG1'
         self.last_update = 0
         pygame.font.init()
         self.font = pygame.font.Font(None, 32)
         self.game_font = pygame.font.Font(None, 48)
-        self.block_images = list()
+        self.block_images = dict()
         self.vegetation_images = dict()
+        
+        self.rectangle_draw_time = pygame.time.get_ticks()
     
     def CreateBlock(self):
         """Creates blocks based on the assigned letter:\n
@@ -37,11 +39,37 @@ class Game:
             for j, tile in enumerate(row):
                 match tile:
                     case 'g':
-                        Block(self, j * 40, i * 40, False, 0)
+                        Block(self, j * 40, i * 40)
+                    case 'w':
+                        Block(self, j * 40, i * 40, has_sides=False)
+                    case 'k':
+                        Block(self, j * 40, i * 40, rotation='upside_down')
+                    case 'a':
+                        Block(self, j * 40, i * 40, rotation='left')
+                    case 'f':
+                        Block(self, j * 40, i * 40, rotation='right')
+                    case 'l':
+                        Block(self, j * 40, i * 40, border=True)
+                    case 'y':
+                        Block(self, j * 40, i * 40, intersection=True)
+                    case 'v':
+                        Block(self, j * 40, i * 40, intersection=True, rotation='left')
+                    case 'h':
+                        Block(self, j * 40, i * 40, intersection=True, rotation='upside_down')
+                    case 'j':
+                        Block(self, j * 40, i * 40, intersection=True, rotation='right')
+                    case 'u':
+                        Block(self, j * 40, i * 40, border=True, rotation='left')
+                    case 'd':
+                        Block(self, j * 40, i * 40, border=True, rotation='upside_down')
+                    case 'r':
+                        Block(self, j * 40, i * 40, border=True, rotation='right')
                     case 's':
-                        Block(self, j * 40, i * 40, True, 0)
+                        Block(self, j * 40, i * 40, walkthrough=True)
+                    case 'c':
+                        Block(self, j * 40, i * 40, asset_type= 0)
                     case 't':
-                        Block(self, j * 40, i * 40, False, 10)
+                        Block(self, j * 40, i * 40, asset_type= 1)
                     case 'p':
                         self.player = Player(self, j * 40, i * 40)
                     case 'e':
@@ -82,7 +110,7 @@ class Game:
 
     def draw(self):
         # Game Loop - Draw
-        self.screen.fill(BLACK)
+        self.screen.fill(BG_COLORS[0])
         self.draw_background()
         self.all_sprites.draw(self.screen)
         self.screen.blit(self.font.render(f'FPS: {self.FPS}', True, GREEN), (0, 0))
@@ -90,9 +118,13 @@ class Game:
         pygame.display.update()
     
     def draw_background(self):
-        for position_offset in range(15):
-            for background in self.background:
-                background.draw(position_offset)
+        # for position_offset in range(15):
+        #     for background in self.background:
+        #         background.draw(position_offset)
+        # timing = random.randint
+        if pygame.time.get_ticks() - self.rectangle_draw_time >= 100:
+            Background_rectangles(self)
+            self.rectangle_draw_time = pygame.time.get_ticks()
     
     def load_background(self):
         "Loads the background layers, only called once"
@@ -109,20 +141,36 @@ class Game:
             offset = (layer_num / total_layer_num)
             Background_Layer(self, pygame.transform.scale(layers, (adjusted_layers_width, WIN_HEIGHT)), offset, layer_num, WIN_WIDTH)
         
+        block_images = list()
         for block_image in os.listdir("IMG/Environment/01/Blocks"):
-            if block_image != "states.png":
+            if "block_" in block_image:
                 image = pygame.image.load(f'IMG/Environment/01/Blocks/{block_image}').convert_alpha()
-                self.block_images.append(image)
+                block_images.append(image)
+        border_image = pygame.image.load(f'IMG/Environment/01/Blocks/Border/border_01.png').convert_alpha()
+        intersection_image = pygame.image.load(f'IMG/Environment/01/Blocks/Border/intersection_01.png').convert_alpha()
+        self.block_images['regular'] = block_images 
+        self.block_images['border'] = border_image
+        self.block_images['intersection'] = intersection_image
                 
         tree_images = list() 
         for tree_image in os.listdir("IMG/Environment/01/Tree"):
-            if tree_image != "states.png" and tree_image != "palette.pal":
+            if "tree_" in tree_image:
                 image = pygame.image.load(f'IMG/Environment/01/Tree/{tree_image}').convert_alpha()
                 scale_ratio_width = TREE_WIDTH / image.get_width()
                 scale_ratio_height = TREE_HEIGHT / image.get_height()
                 image = pygame.transform.scale(image, ((image.get_width() * scale_ratio_width, image.get_height() * scale_ratio_height)))
                 tree_images.append(image)
         self.vegetation_images[TREE] = tree_images
+        
+        bush_images = list() 
+        for bush_image in os.listdir("IMG/Environment/01/Bush"):
+            if "bush_" in bush_image:
+                image = pygame.image.load(f'IMG/Environment/01/Bush/{bush_image}').convert_alpha()
+                scale_ratio_width = BUSH_WIDTH / image.get_width()
+                scale_ratio_height = BUSH_HEIGHT / image.get_height()
+                image = pygame.transform.scale(image, ((image.get_width() * scale_ratio_width, image.get_height() * scale_ratio_height)))
+                bush_images.append(image)
+        self.vegetation_images[BUSH] = bush_images
 
     def main(self):
         while self.playing:
